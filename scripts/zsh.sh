3a46d42076fdf1e281c056663d28109bc0c86f1b
ochairo <19258508+ochairo@users.noreply.github.com>
2024-02-24 02:18:54 +0900

    Update zsh.sh

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -3,15 +3,15 @@
 echo "zsh.sh"
 
 brew install zsh
+
+brew install zsh-autosuggestions
+brew install zsh-syntax-highlighting
+brew install zsh-completions
+
 brew install powerlevel10k
 
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.zsh/plugins/zsh-users/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/plugins/zsh-users/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions.git $HOME/.zsh/plugins/zsh-users/zsh-completions
-
-ln -nfs $REPO_DIR/dotfiles/config/.zshenv $HOME/.zshenv
-ln -nfs $REPO_DIR/dotfiles/config/.zshrc $HOME/.zshrc
-ln -nfs $REPO_DIR/dotfiles/config/.zlogin $HOME/.zlogin
-ln -nfs $REPO_DIR/dotfiles/config/.zlogout $HOME/.zlogout
+# Avoid warnings when attempting to load completions
+chmod go-w '/opt/homebrew/share'
+chmod -R go-w '/opt/homebrew/share/zsh'
 
 exit 0
